b9bb0049a6ac14f9e46d0b820ec38f1709bbabf1
Pavel Kukushkin <mail@pavel.online>
2021-01-24 14:06:42 +0100

    do not use helm in lsp and use global which key

--- emacs/lsp.el
+++ emacs/lsp.el
@@ -11,8 +11,6 @@
 
 ;; optionally
 (use-package lsp-ui :commands lsp-ui-mode)
-;; if you are helm user
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
 ;; if you are ivy user
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
@@ -20,8 +18,3 @@
 ;; optionally if you want to use debugger
 (use-package dap-mode)
 ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
-;; optional if you want which-key integration
-(use-package which-key
-    :config
-    (which-key-mode))
