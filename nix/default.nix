83b033a5fbae823f3e38491fc3561aadafe5b991
Pavel Kukushkin <mail@pavel.online>
2021-01-25 20:45:43 +0100

    Bluez configuration

--- nix/default.nix
+++ nix/default.nix
@@ -91,6 +91,8 @@ in
 
   hardware.bluetooth = {
     enable = true;
+    package = pkgs.bluezFull;
+    powerOnBoot = false;
     config = {
       General = {
         Enable = "Source,Sink,Media,Socket";
